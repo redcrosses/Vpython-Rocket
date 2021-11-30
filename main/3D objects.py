8e0484ca2edb21dccb3708e34116ee32304bb378
redcrosses <94404359+redcrosses@users.noreply.github.com>
2021-11-30 21:16:13 +0200

    MORE GRAPHS

--- main/3D objects.py
+++ main/3D objects.py
@@ -34,13 +34,19 @@ t = 0.102 #time between readings
 y = 0 #total time
 
 scene = canvas(width = 1000, height = 1000, background = color.white, align = "left")
-sphereVector = vector(0,0,0)
 
-sphere1 = sphere(pos = sphereVector, radius = 3, color = color.orange, make_trail=True)
 
-xarr = arrow(pos=sphere1.pos, axis=vector(accXx,0,0), shaftwidth = .75, headwidth = 1, color=color.red, round=True) 
-yarr = arrow(pos=sphere1.pos, axis=vector(0,accXy,0), shaftwidth = .75, headwidth = 1, color=color.green, round=True) 
-zarr = arrow(pos=sphere1.pos, axis=vector(0,0,accXz), shaftwidth = .75, headwidth = 1, color=color.blue, round=True) 
+l = 5 #length of rocket element
+
+rocketVector = vector(0,0,0)
+previousVector = vector(0,0,0)
+rocket = box(pos = rocketVector, height = 3, width = 3, axis = vector(3, 0, 0) , color = color.orange, make_trail=True)
+
+
+
+xarr = arrow(pos=rocket.pos, axis=vector(accXx,0,0), shaftwidth = .75, headwidth = 1, color=color.red, round=True) 
+yarr = arrow(pos=rocket.pos, axis=vector(0,accXy,0), shaftwidth = .75, headwidth = 1, color=color.green, round=True) 
+zarr = arrow(pos=rocket.pos, axis=vector(0,0,accXz), shaftwidth = .75, headwidth = 1, color=color.blue, round=True) 
 
 g1 = graph(width=800,height=200,title='X acceleration', fast=False, align = "right")
 f1 = gcurve(color = color.red) # graphics curve x
@@ -53,6 +59,12 @@ f2 = gcurve(color = color.green) #graphics curve y
 
 g22 = graph(width=800, height=200, title='Y displacement', fast=False, align = "right")
 f22 = gcurve(color = color.orange)
+
+g3 = graph(width=800, height=200, title='Z Acceleration', fast=False, align = "right")
+f3 = gcurve(color = color.purple)
+
+g32 = graph(width=800, height=200, title='Z displacement', fast=False, align = "right")
+f32 = gcurve(color = color.black)
 
 # sample of data: "  -1184   3120 -15824"
 
@@ -113,23 +125,24 @@ def filteredLine(line):
         # conversion factor of 16, because the lowest 4 bits of the accelerometer's readings are 0.
 
 
-    accX = (float(accX) / -16 / 1000 * 9.81) * 100 - previousx #cm/s^2
-    accY = (float(accY) / -16 / 1000 * 9.81) * 100 - previousy #cm/s^2
+    accX = (float(accX) / -16 / 1000 * 9.81) - previousx #m/s^2
+    accY = (float(accY) / -16 / 1000 * 9.81) - previousy #m/s^2
 
-    accZ = (float(accZ) / -16 / 1000 * 9.81) * 100 - previousz #cm/s^2
+    accZ = (float(accZ) / -16 / 1000 * 9.81) - previousz #m/s^2
 
     previousx = accX
     previousy = accY
     previousz = accZ
 
-    accXy = accY #(float(accX)*sin(math.radians(float(pitch))))
-    accXx = accX #(float(accX)*cos(math.radians(float(pitch)))*cos(math.radians(float(yaw))))
-    accXz = accZ #(float(accX)*cos(math.radians(float(pitch)))*sin(math.radians(float(yaw))))
+    accXy = accY * 100 #(float(accX)*sin(math.radians(float(pitch)))) #cm/s^2
+    accXx = accX  * 100 #(float(accX)*cos(math.radians(float(pitch)))*cos(math.radians(float(yaw)))) #cm/s^2
+    accXz = accZ * 100  #(float(accX)*cos(math.radians(float(pitch)))*sin(math.radians(float(yaw)))) #cm/s^2
 
 
 for x in f:
+    timerun = time.time()
     filteredLine(x)
-    # sphereVector = vector(int(accX), int(accY), int(accZ))  
+    # rocketVector = vector(int(accX), int(accY), int(accZ))  
 
 
     sx = (ux*t)+(0.5*accXx)*t**2
@@ -140,34 +153,44 @@ for x in f:
     uy = uy + accXy*t
     uz = uz + accXz*t
 
-    f1.plot(y, accXx)
+    f1.plot(y, accX)
     f12.plot(y, sx)
-    f2.plot(y, accXy)
+    f2.plot(y, accY)
     f22.plot(y, sy)
-
-    sphere1.pos = vector(sx, sy, sz)
+    f3.plot(y, accZ)
+    f32.plot(y, sz)
 
     xarr.pos = vector(sx, sy, sz)
     yarr.pos = vector(sx, sy, sz)
     zarr.pos = vector(sx, sy, sz)
+
+    # if int(timerun) % 2 == 0:
+    #     rocket.axis = rocket.pos
+
+    rocket.pos = vector(sx, sy, sz)
+
+    
+    
+    
 
     xarr.axis = vector(accXx*t,0,0)
     yarr.axis = vector(0,accXy*t,0)
     zarr.axis = vector(0,0,accXz*t)
     
     y = y + t
+    
 
-    # sphereVector.x = sx
-    # sphereVector.y = sy
-    # sphereVector.z = sz
+    # rocketVector.x = sx
+    # rocketVector.y = sy
+    # rocketVector.z = sz
 
-    # sphere1.pos = sphereVector
-    # arrow1.pos = sphereVector
-    scene.camera.follow(sphere1)
+    # rocket.pos = rocketVector
+    # arrow1.pos = rocketVector
+    scene.camera.follow(rocket)
 
 
     print(sx, sy, sz, pitch, roll, yaw)
-    time.sleep(0.05)
+    time.sleep(t)
     
 
 
